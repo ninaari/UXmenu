1bbcf5e1b856658be6dbb70491490f058a0e1912
Gav <gavin@woodsofplenty.com>
2021-05-24 12:40:28 -0700

    added a bunch of methods/updated the download function

--- toolkit.js
+++ toolkit.js
@@ -43,22 +43,21 @@ $('.toggle').on('click', function(){
       $('.'+cata + "> .nothing").text('Nothing Selected');
     }
     
-    //trying to do something with a diabled chekbox
+    //trying to do something with a diabled chekbox, can't select cause it is disabled
     let defaultD = $(this).attr('data-default');
     // if(defaultD){
     //   alert('whaaaa?');
     // }
     
-    let inCart = $(":checkbox:checked").length;
-    $('.totalNum').text(inCart);
-    let inDisc = $(".discChecks:checkbox:checked").length;
-    $('.discNum').text(inDisc);
-    let inDefi = $(".defChecks:checkbox:checked").length;
-    $('.defNum').text(inDefi);
-    let inIdea = $(".ideaChecks:checkbox:checked").length;
-    $('.ideaNum').text(inIdea);
-    let inTest = $(".testChecks:checkbox:checked").length;
-    $('.testNum').text(inTest);
+    //update total in cart and sections
+    itemNum();
+
+    //for the download button
+    let description = $(this).parentsUntil('.content');
+    // description.css('border','2px dotted red')
+    //alert(description.not('.material-icons').text());
+    let dText = description.contents().not("span").text();
+    $('#dlContent').append(dText + '<br>');
     
   });
   
@@ -79,18 +78,32 @@ $('.toggle').on('click', function(){
 
     //remove it
     $(this).parent().remove();
-    //reduce total in cart
-    let inCart = $(":checkbox:checked").length;
-    $('.totalNum').text(inCart);
+
+    //reduce total in cart and sections
+    itemNum();
 
     //Subtract time from total
     let totalTime = parseInt($('#time').val());
     let time = parseInt($(this).parent().data('time'));
     $('#time').val(totalTime-time);
-
-    //
     
   });
+
+  function itemNum(){
+        //reduce total in cart
+        let inCart = $(":checkbox:checked").length;
+        $('.totalNum').text(inCart);
+    
+        //update section number
+        let inDisc = $(".discChecks:checkbox:checked").length;
+        $('.discNum').text(inDisc);
+        let inDefi = $(".defChecks:checkbox:checked").length;
+        $('.defNum').text(inDefi);
+        let inIdea = $(".ideaChecks:checkbox:checked").length;
+        $('.ideaNum').text(inIdea);
+        let inTest = $(".testChecks:checkbox:checked").length;
+        $('.testNum').text(inTest);
+  }
   
   //NAVIGATION
   $('#start').on('click', function(){
@@ -158,3 +171,20 @@ $('.toggle').on('click', function(){
   });
   
 //.discoveries .defines .ideas .tests
+
+//DOWNLOAD CONTENT
+function downloadInnerHtml(filename, elId, mimeType) {
+  var elHtml = document.getElementById(elId).innerHTML;
+  var link = document.createElement('a');
+  mimeType = mimeType || 'text/plain';
+
+  link.setAttribute('download', filename);
+  link.setAttribute('href', 'data:' + mimeType + ';charset=utf-8,' + encodeURIComponent(elHtml));
+  link.click(); 
+}
+
+var fileName =  'Liberty-UX-Toolkit.txt'; 
+
+$('#downloadLink').click(function(){
+  downloadInnerHtml(fileName, 'dlContent','text/html');
+});
